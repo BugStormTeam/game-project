fa7fa960fd6fb42966d98214caad92e7bc467f8c
walkoda <walkoda@gmail.com>
2018-06-13 12:10:39 +0200

    First push.

---
+++ desktop/src/com/bugstorm/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bugstorm.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bugstorm.game.GameProject;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameProject(), config);
+	}
+}
